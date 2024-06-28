72813792cac3f86b2914e3a6bab7a54a4966fb9b
Arvin-Hacks <boatgamer0980@gmail.com>
2024-06-28 08:30:06 +0530

    Order Controller

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -39,3 +39,54 @@ export const placeOrder = async (req, res) => {
     return res.status(500).json({ message: "Server error", error });
   }
 };
+
+// New function to get the cart items for the logged-in user
+export const getOrders = async (req, res) => {
+  // const userId = req.user._id;
+  const userId = req.query?.userId;
+
+  //   console.log("req", req);
+
+  console.log("userId", req?.body);
+  //   let id = new mongoose.Types.ObjectId(userId);
+  console.log(userId);
+
+  try {
+    const orders = await Order.find({ user: userId }).populate("items.product");
+
+    console.log("orders", orders);
+
+    if (!orders) {
+      return res.status(404).json({ message: "Orders not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "SuccessFully Fetched orders", data: orders });
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching Order", error });
+  }
+};
+
+// New function to get the cart items for the logged-in user
+export const getOrderDetails = async (req, res) => {
+  try {
+    const { id } = req?.params;
+    console.log("ID", id);
+    if (!id) {
+      return res
+        .status(400)
+        .json({ message: "Inavalid order Details Request!" });
+    }
+
+    const response = await Order.findById(id).populate("items.product");
+    if (!response) {
+      return res.status(404).json({ message: "Orders not found" });
+    }
+
+    return res.status(200).json({ message: "Success", data: response });
+  } catch (error) {
+    console.log("get Error", error);
+    return res.status(500).json({ message: "Order Details get error", error });
+  }  
+};
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { placeOrder } from '../controllers/order.controllers.js';
+import { placeOrder,getOrderDetails,getOrders } from '../controllers/order.controllers.js';
 
 const router = express.Router();
 
-// router.get('/', addRequestMessages)
+router.get('/', getOrders)
 router.post('/', placeOrder)
-// router.get('/request-details/:id', getReqestDeatilsById)
+router.get('/order-details/:id', getOrderDetails)
 // router.post('/create-request', upload.array('images', 10), createRequestProduct); // Adjust the second parameter for the maximum number of images allowed
 
 export default router;
